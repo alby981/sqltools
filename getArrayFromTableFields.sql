e47863096e4ebc740345db902bbac18977490dec
Alberto Belotti <albertopuntobelotti@gmail.com>
2019-01-17 12:06:35 +0100

    Get PHP array of fields defined in the database

---
+++ getArrayFromTableFields.sql
@@ -0,0 +1,15 @@
+delimiter //
+drop procedure getArrayFromTableFields;
+
+CREATE PROCEDURE getArrayFromTableFields(IN TABLE_NAM VARCHAR(128), IN DB VARCHAR(128))
+ SELECT 
+ concat("$aArray = ['",REPLACE(group_concat(`COLUMN_NAME`), ',', "','"),"'];") as aFields
+ FROM information_schema.`COLUMNS`
+ WHERE `TABLE_NAME` = TABLE_NAM
+ and table_schema = DB;
+END //
+delimiter ;
+
+
+call getArrayFromTableFields('profiles', 'cockpit_baybets_net_new');
+
